95b7d268ace83b248ed02799895528231fdc55ab
Drkainat8 <161299000+Drkainat8@users.noreply.github.com>
2024-06-15 09:38:58 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+//assignment 22
+//make an intenstional error and correct it before closing the program
+var favicecreamfalavors = ["mango", "vanilla", "choclate", "pista", "kulfa", "rose"];
+//now make an intentional error
+console.log(favicecreamfalavors[8]);
+//now make a correct version
+console.log(favicecreamfalavors[4]);
---
+++ main.ts
@@ -0,0 +1,7 @@
+//assignment 22
+//make an intenstional error and correct it before closing the program
+let favicecreamfalavors : string[] =[`mango`,`vanilla`,`choclate`,`pista`,`kulfa`,`rose`]
+//now make an intentional error
+console.log(favicecreamfalavors[8])
+//now make a correct version
+console.log(favicecreamfalavors[4])
